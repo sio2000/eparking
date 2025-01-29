8ae346b2eb18d2eebb79dfd7f1a43bb5704c9f77
sio2000 <karapanosnikos9@gmail.com>
2025-01-28 20:33:02 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/eparking/', // Για GitHub Pages, το repo είναι στο φάκελο /eparking/
+  base: './',  // Αυτό είναι για να δουλέψει το relative path κατά τη διάρκεια του build
 
 })
