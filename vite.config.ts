a7d1201baca766fa472d40b0a9bb22482bb47b76
sio2000 <karapanosnikos9@gmail.com>
2025-01-28 20:07:29 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/eparking/', // Προσθήκη του base path
-})
+  base: '/eparking/',  // Η διαδρομή του repository σου
+});
